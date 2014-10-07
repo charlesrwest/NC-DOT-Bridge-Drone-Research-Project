89a621935520338057baae06f695923caa86e143
Charles West <crwest@ncsu.edu>
2014-10-07 10:03:14 -0400

    Some notes and work on camera calibration.

---
+++ code/cameraCalibration/opencvCameraCalibrationExample/CMakeFiles/cameraCalibration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cameraCalibration.dir/camera_calibration.cpp.o"
+  "cameraCalibration.pdb"
+  "cameraCalibration"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cameraCalibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
